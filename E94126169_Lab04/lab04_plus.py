4df36534d3ab7c5f946cdc635c2046bb2a4e4c07
Sean95164 <89783828+Sean95164@users.noreply.github.com>
2023-09-25 18:46:06 +0800

    Update lab04_plus.py

--- E94126169_Lab04/lab04_plus.py
+++ E94126169_Lab04/lab04_plus.py
@@ -1,18 +1,17 @@
-nums_1 = []
-nums_2 = []
+nums_list = []
 nums_string = input("輸入的list為:") #例如:輸入[3, 2, 2, 3, 6, 5, 4, 3, 2, 1]
 del_num = int(input("要刪除的數字是:"))
 print(f"輸入的list為:{nums_string}, 要刪除的數字是:{del_num}")
 
 for i in nums_string[1:len(nums_string)-1].split(","):
-    nums_1.append(int(i))
-
+    if(i!=""):
+        nums_list.append(int(i))
+    
 print()
 print("刪除後!")
 print()
 
-for i in nums_1:
-    if(i!=del_num):
-        nums_2.append(i)
+while(nums_list.count(del_num)):
+    nums_list.remove(del_num)
         
-print(f"list長度剩下:{len(nums_2)}, list變成:{nums_2}")
+print(f"list長度剩下:{len(nums_list)}, list變成:{nums_list}")
