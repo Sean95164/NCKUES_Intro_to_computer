45ca58f9d01ef3286377f19ac222484c86a0be34
Sean95164 <89783828+Sean95164@users.noreply.github.com>
2023-10-02 18:33:17 +0800

    Update lab05_plus.py

--- E94126169_Lab05/lab05_plus.py
+++ E94126169_Lab05/lab05_plus.py
@@ -8,6 +8,7 @@ for i in range(4):
     # 將輸入的 values 以逗號做分割，並以 list 做儲存
     values = list(input("Enter values: ").split(","))
     print(f"Enter values: {values}")
+    # 將 key-value pair 放入 dict 中
     dict0[key] = values
     
 # 用 f-string 輸出字典的內容
