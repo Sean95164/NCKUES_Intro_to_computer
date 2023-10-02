18e8f667fc6ebcf4aaecffb19cfb2a5edf587ab7
Sean95164 <89783828+Sean95164@users.noreply.github.com>
2023-10-02 18:32:11 +0800

    Add files via upload

---
+++ E94126169_Lab05/lab05_plus.py
@@ -0,0 +1,14 @@
+# 建立一個空的字典
+dict0 = {}
+
+# 輸入資料並儲存至dict
+for i in range(4):
+    key = input("Enter Keys: ")
+    print(f"Enter Keys: {key}")
+    # 將輸入的 values 以逗號做分割，並以 list 做儲存
+    values = list(input("Enter values: ").split(","))
+    print(f"Enter values: {values}")
+    dict0[key] = values
+    
+# 用 f-string 輸出字典的內容
+print(f"dict0 = {dict0}")
