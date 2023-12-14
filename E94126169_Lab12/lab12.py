631590d35d2e82f14a7ae33f6b32fef247d379f8
Sean <swsc940106@gmail.com>
2023-12-14 19:15:54 +0800

    added comment

--- E94126169_Lab12/lab12.py
+++ E94126169_Lab12/lab12.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# 讀取檔案並過濾資料
 def read_data(filename):
     data = []
     with open(filename) as file:
@@ -11,20 +12,25 @@ def read_data(filename):
         
     return data
 
+# 繪製圖表一, 每年各月的溫度折線圖
 def Monly_temperature_figure1(data, ax):
     month = np.linspace(1,12,12)
+
+    # 設定座標軸標題, 圖表標題, x軸刻度
     ax.set_xlabel("Month")
     ax.set_ylabel("Temperature in Degree C")
     ax.set_title("Tainan Monthly Mean Temperature From 2013 To 2021")
     ax.set_xticks(month)
     
 
+    # 繪製各年度的折線圖
     for i, year_temperature in enumerate(data):
         ax.plot(month, year_temperature, label=2013+i)
+
+    # 設定圖例
     ax.legend(loc="lower center")
     
-    
-
+# 計算各月的年平均溫度數值    
 def the_average_temperature_each_month(data):
     months_average = np.linspace(0,0,12)
     for arr in data:
@@ -34,8 +40,11 @@ def the_average_temperature_each_month(data):
     nine_years_mean = np.mean(months_average)
     return np.around(months_average, 2), np.around(nine_years_mean, 2)
       
+# 繪製圖表二, 各月的年平均溫度
 def Monly_temperature_figure2(data, ax):
     month = np.linspace(1,12,12)
+
+    # 設定坐標軸標題, 圖表標題, x軸刻度, y軸刻度的上下限
     ax.set_xlabel("Month")
     ax.set_ylabel("Temperature in Degree C")
     ax.set_title("Tainan Monthly Mean Temperature From 2013 To 2021")
@@ -44,11 +53,15 @@ def Monly_temperature_figure2(data, ax):
 
     months_average, total_average = the_average_temperature_each_month(data)
 
+    # 繪製折線圖和散佈圖
     ax.plot(month, months_average)
     ax.scatter(month, months_average, color="red")
+
+    # 標記數值
     for i, label in enumerate(months_average):
         ax.text(month[i], months_average[i], label)
     
+    # 繪製各月平均溫度水平線
     ax.axhline(total_average, color="red", linestyle="--", label="Mean of 9 Years")
     ax.text(1,25,total_average)
     ax.legend()
@@ -58,7 +71,6 @@ def Monly_temperature_figure2(data, ax):
     
 def main():
     data = read_data(filename="Temperature.txt")
-    # fig, ax = plt.subplots()
     fig, ax = plt.subplots(1,2, figsize=(15,6))
     Monly_temperature_figure1(data, ax[0])
     Monly_temperature_figure2(data, ax[1])
--- E94126169_Lab12/lab12_plus.py
+++ E94126169_Lab12/lab12_plus.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
+# 讀取檔案和過濾資料
 def read_data(filename):
     data_x = []
     data_y = []
@@ -16,6 +17,7 @@ def read_data(filename):
             data_y.append(y)
     return np.asarray(data_x), np.asarray(data_y)
 
+# 繪製散佈圖和設定標題、軸的文字
 def oddExperiment_scatter(data, ax):
     x_ticks = np.arange(-10.0, 10.1, 2.5)
     ax.set_title("oddExperiment Data")
@@ -26,54 +28,70 @@ def oddExperiment_scatter(data, ax):
     line = ax.scatter(x, y, label="Data")
     return line
 
+# 計算一次趨勢線的方程式, 並回傳
 def find_linear_trend_line_equation(data):
     x, y = data
     coefficients = np.polyfit(x, y, 1)
     return np.poly1d(coefficients)
 
+# 計算二次趨勢線的方程式, 並回傳
 def fine_square_trend_line_equation(data):
     x, y = data
     coefficients = np.polyfit(x, y, 2)
     return np.poly1d(coefficients)
 
+# 繪製一次趨勢線的圖形
 def trend_line_dg1(data, ax):
     x = np.linspace(-10.0, 10.0, 100)
     func = find_linear_trend_line_equation(data)
     y = func(x) 
     ax.plot(x, y, color="red")
 
+# 繪製二次趨勢線的圖形
 def trend_line_dg2(data, ax):
     x = np.linspace(-10.0, 10.0, 100)
     func = fine_square_trend_line_equation(data)
     y = func(x)
     ax.plot(x, y, color="purple")
 
+# 計算 LSE, 並且回傳數值
 def LSE_and_RSquare(data, equation):
     x_p, y_p = data
-    mean = np.mean(y_p)
     y_i = equation(x_p)
     diff_square = (y_i - y_p)**2
     LSE = np.mean(diff_square)
-    RSquare = 1 - (np.sum(diff_square)/np.sum((y_p-mean)**2))
-    return np.around(LSE, 5), np.around(RSquare, 5)
+    # mean = np.mean(y_p)
+    # RSquare = 1 - (np.sum(diff_square)/np.sum((y_p-mean)**2))
+    return np.around(LSE, 5)
 
-def calculate_and_set_legent(data, ax, line):
-    LSE1, R2_1 = LSE_and_RSquare(data, find_linear_trend_line_equation(data))
-    LSE2, R2_2 = LSE_and_RSquare(data, fine_square_trend_line_equation(data))
+# 計算 LSE 和設定 legend
+def calculate_and_set_legend(data, ax, line):
+    # 計算一次趨勢線的 LSE
+    LSE1 = LSE_and_RSquare(data, find_linear_trend_line_equation(data))
+    # 計算二次趨勢線的 LSE
+    LSE2 = LSE_and_RSquare(data, fine_square_trend_line_equation(data))
+    # 繪製 legend
     orange_line = mlines.Line2D([],[], color="orange", label=f"Fit of degree 1, LSE = {LSE1}")
     green_line = mlines.Line2D([],[], color="green", label=f"Fit of degree 2, LSE = {LSE2}")
-    red_line = mlines.Line2D([],[], color="red", label=f"Fit of degree 1, R2 = {R2_1}")
-    purple_line = mlines.Line2D([],[], color="purple", label=f"Fit of degree 2, R2 = {R2_2}")
-    text = [line, orange_line, green_line, red_line, purple_line]
+    # red_line = mlines.Line2D([],[], color="red", label=f"Fit of degree 1, R2 = {R2_1}")
+    # purple_line = mlines.Line2D([],[], color="purple", label=f"Fit of degree 2, R2 = {R2_2}")
+    text = [line, orange_line, green_line]
     ax.legend(handles=text, loc="upper center")
 
 def main():
+    # 進行資料處理
     data = read_data(filename="oddExperiment.txt")
+    # 創造子圖
     fig, ax = plt.subplots()
+    # 繪製散佈圖
     line = oddExperiment_scatter(data, ax)
+    # 繪製一次趨勢線
     trend_line_dg1(data, ax)
+    # 繪製二次趨勢線
     trend_line_dg2(data, ax)
-    calculate_and_set_legent(data,ax,line)
+    # 計算 LSE 和製作 legend
+    calculate_and_set_legend(data,ax,line)
+    # 儲存圖檔
     fig.savefig("lab12_plus.png")
     
 main()
